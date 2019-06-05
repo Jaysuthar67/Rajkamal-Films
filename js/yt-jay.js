376445a45cd7f9d93a6c9898a5e7da5970b2a8f7
jay-suthar <jaysuthar67@gmail.com>
2019-06-05 20:04:23 +0530

    First commit

---
+++ js/yt-jay.js
@@ -0,0 +1,16 @@
+/*
+<iframe
+  width="1349"
+  height="480"
+  src="https://www.youtube.com/embed/qk4hG_3IRG8"
+  frameborder="0"
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen
+/>;
+
+
+
+
+
+*/
+
