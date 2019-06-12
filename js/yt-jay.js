a1d9f2f496cf1ac24e96e5e4cfe5adfd5541e169
jay-suthar <jaysuthar67@gmail.com>
2019-06-12 16:24:32 +0530

    ..

--- js/yt-jay.js
+++ js/yt-jay.js
@@ -26,13 +26,3 @@ function preventDefaultAcrions(event) {
   }
 }
 
-var docElm = document.documentElement;
-if (docElm.requestFullscreen) {
-  docElm.requestFullscreen();
-} else if (docElm.mozRequestFullScreen) {
-  docElm.mozRequestFullScreen();
-} else if (docElm.webkitRequestFullScreen) {
-  docElm.webkitRequestFullScreen();
-} else if (docElm.msRequestFullscreen) {
-  docElm.msRequestFullscreen();
-}
