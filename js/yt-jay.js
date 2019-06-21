1dc8169550e1569253253c1b8d23c744985519e1
jaysuthar67 <jaysuthar67@gmail.com>
2019-06-21 20:30:08 +0530

    ..

--- js/yt-jay.js
+++ js/yt-jay.js
@@ -1,22 +1,6 @@
-/*
-<iframe
-  width="1349"
-  height="480"
-  src="https://www.youtube.com/embed/qk4hG_3IRG8"
-  frameborder="0"
-  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-  allowfullscreen
-/>;
+document.oncontextmenu = preventDefaultActions;
 
-
-
-
-
-*/
-
-document.oncontextmenu = preventDefaultAcrions;
-
-function preventDefaultAcrions(event) {
+function preventDefaultActions(event) {
   event = event || window.event;
   if (event.preventDefault) {
     event.preventDefault()
@@ -27,7 +11,7 @@ function preventDefaultAcrions(event) {
 }
 
 document.onkeydown = function (e) {
-  if (event.keyCode == 123) {
+  if (e.keyCode == 123) {
     return false;
   }
   if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
@@ -35,7 +19,8 @@ document.onkeydown = function (e) {
   }
   if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
     return false;
-   }
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+   return false;
+  }
 }
-
-
